6ad20c1406781777b2b91fd7dcd900ad9821f920
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-09-13 22:02:40 -0300

    feat: install components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
 
   return (
-    <div>Felipe</div>
+    <div>Felipe <Button variant="destructive">Subscribe</Button></div>
   )
 }
 
