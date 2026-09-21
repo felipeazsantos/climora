f783dbedc2c515b13a41f3aabbfb5f9ffdee258d
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-09-21 16:21:45 -0300

    feat: Weather Skeleton

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,9 +1,39 @@
-
+import { Button } from "@/components/ui/button";
+import { useGeolocation } from "@/hooks/use-geolocation";
+import { RefreshCw } from "lucide-react";
 
 const WeatherDashboard = () => {
-    return (
-        <div>WeatherDashboard</div>
-    )
-}
+    const {
+        coordinates,
+        error: locaitonError,
+        getLocation,
+        isLoading: locationLoading,
+    } = useGeolocation();
 
-export default WeatherDashboard
+    const handleRefresh = () => {
+        getLocation();
+        if (coordinates) {
+            //reload weather data
+        }
+    };
+
+    if (locationLoading) {
+        
+    }
+
+    return (
+        <div>
+            {/* Favorite Cities */}
+            <div className="flex items-center justify-between">
+                <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+                <Button variant="outline" size={"icon"}>
+                    <RefreshCw />
+                </Button>
+            </div>
+
+            {/* Current and Hourly weather */}
+        </div>
+    );
+};
+
+export default WeatherDashboard;
