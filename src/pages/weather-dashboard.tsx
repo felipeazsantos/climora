e2439a31723594113ccbc0e5ae31bedcd04f1e8c
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-09-21 16:33:02 -0300

    feat: threating error cases

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,11 +1,13 @@
+import WeatherSkeleton from "@/components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { useGeolocation } from "@/hooks/use-geolocation";
-import { RefreshCw } from "lucide-react";
+import { AlertCircle, MapPin, RefreshCw } from "lucide-react";
 
 const WeatherDashboard = () => {
     const {
         coordinates,
-        error: locaitonError,
+        error: locationError,
         getLocation,
         isLoading: locationLoading,
     } = useGeolocation();
@@ -18,7 +20,38 @@ const WeatherDashboard = () => {
     };
 
     if (locationLoading) {
-        
+        return <WeatherSkeleton />;
+    }
+
+    if (locationError) {
+        return (
+            <Alert variant="destructive">
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Location Error</AlertTitle>
+                <AlertDescription className="flex flex-col gap-4">
+                    <p>{locationError}</p>
+                    <Button onClick={getLocation} variant="outline" className="w-fit">
+                        <MapPin className="mr-2 h-4 w-4" />
+                        Enable Location
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        );
+    }
+
+    if (!coordinates) {
+        return (
+            <Alert variant="destructive">
+                <AlertTitle>Location Required</AlertTitle>
+                <AlertDescription className="flex flex-col gap-4">
+                    <p>Please enable location acess to see your local weather.</p>
+                    <Button onClick={getLocation} variant="outline" className="w-fit">
+                        <MapPin className="mr-2 h-4 w-4" />
+                        Enable Location
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        );
     }
 
     return (
@@ -26,7 +59,7 @@ const WeatherDashboard = () => {
             {/* Favorite Cities */}
             <div className="flex items-center justify-between">
                 <h1 className="text-xl font-bold tracking-tight">My Location</h1>
-                <Button variant="outline" size={"icon"}>
+                <Button onClick={handleRefresh} variant="outline" size={"icon"}>
                     <RefreshCw />
                 </Button>
             </div>
